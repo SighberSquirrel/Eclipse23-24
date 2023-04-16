9ff2556b2768f863cf46907416f54e879b4b6fce
SighberSquirrel <130690424+SighberSquirrel@users.noreply.github.com>
2023-04-16 11:46:04 -0400

    created a java project and a unit1 folder

---
+++ unit1/src/unit1/exampleProgram.java
@@ -0,0 +1,10 @@
+package unit1;
+
+public class exampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is an example");
+	}
+
+}
